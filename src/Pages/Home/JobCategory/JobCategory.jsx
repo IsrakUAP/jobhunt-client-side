b431dc0bda33b67a1813c544fc0f462bf55fd63f
IsrakUAP <israk2301@gmail.com>
2023-11-08 20:15:11 +0600

    react spinner added in loading state in home page showing data after loading

--- src/Pages/Home/JobCategory/JobCategory.jsx
+++ src/Pages/Home/JobCategory/JobCategory.jsx
@@ -1,10 +1,22 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 const JobCategory = () => {
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+
+    },700)
+  },[])
+
+
     const [jobs, setJobs] = useState([]);
     useEffect(()=>{
         fetch('https://b8a11-server-side.vercel.app/category')
@@ -12,71 +24,85 @@ const JobCategory = () => {
         .then(data => setJobs(data));
     },[])
     return (
+      <div>    {
+        loading?
+        
+        (
+          <div className="flex items-center justify-center">
+            <ClipLoader color="#3498db" loading={loading} size={120} />
+          </div>
+        ) :
+        
+        
         <div className="container mx-auto my-8 p-8">
-             <h1 className="text-3xl font-bold mb-6 text-center">Job Categories</h1>
-        <Tabs>
-          <TabList className="flex space-x-4">
-            <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Web Development</Tab>
-            <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Digital Marketing</Tab>
-            <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Graphics Design</Tab>
-          </TabList>
-  
-          <div className="border-t border-gray-300 mt-4"></div>
-  
-          <TabPanel>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
-            {jobs
-              .filter((job) => job.category === "web development")
-              .map((job) => (
-                <div key={job._id} className="bg-white rounded shadow-lg p-6">
-                  <h2 className="text-xl font-bold mb-2">{job.title}</h2>
-                  <p className="text-gray-600 mb-2">{job.deadline}</p>
-                  <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
-                  <p className="text-gray-700 mb-2">{job.shortDescription}</p>
-                  <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
-                    Bid Now
-                  </button></Link>
-                </div>
-              ))}
-          </div>
-        </TabPanel>
+      
+        <h1 className="text-3xl font-bold mb-6 text-center">Job Categories</h1>
+   <Tabs>
+     <TabList className="flex space-x-4">
+       <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Web Development</Tab>
+       <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Digital Marketing</Tab>
+       <Tab className="text-blue-500 hover:text-blue-700 hover:cursor-pointer">Graphics Design</Tab>
+     </TabList>
 
-        <TabPanel>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
-            {jobs
-              .filter((job) => job.category === "digital marketing")
-              .map((job) => (
-                <div key={job._id} className="bg-white rounded shadow-lg p-6">
-                  <h2 className="text-xl font-bold mb-2">{job.title}</h2>
-                  <p className="text-gray-600 mb-2">{job.deadline}</p>
-                  <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
-                  <p className="text-gray-700 mb-2">{job.shortDescription}</p>
-                  <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
-                    Bid Now
-                  </button></Link>
-                </div>
-              ))}
-          </div>
-        </TabPanel>
+     <div className="border-t border-gray-300 mt-4"></div>
 
-        <TabPanel>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
-            {jobs
-              .filter((job) => job.category === "graphics design")
-              .map((job) => (
-                <div key={job._id} className="bg-white rounded shadow-lg p-6">
-                  <h2 className="text-xl font-bold mb-2">{job.title}</h2>
-                  <p className="text-gray-600 mb-2">{job.deadline}</p>
-                  <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
-                  <p className="text-gray-700 mb-2">{job.shortDescription}</p>
-                  <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
-                    Bid Now
-                  </button></Link>
-                </div>
-              ))}
-          </div>
-        </TabPanel>
-      </Tabs>
+     <TabPanel>
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+       {jobs
+         .filter((job) => job.category === "web development")
+         .map((job) => (
+           <div key={job._id} className="bg-white rounded shadow-lg p-6">
+             <h2 className="text-xl font-bold mb-2">{job.title}</h2>
+             <p className="text-gray-600 mb-2">{job.deadline}</p>
+             <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
+             <p className="text-gray-700 mb-2">{job.shortDescription}</p>
+             <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
+               Bid Now
+             </button></Link>
+           </div>
+         ))}
+     </div>
+   </TabPanel>
+
+   <TabPanel>
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+       {jobs
+         .filter((job) => job.category === "digital marketing")
+         .map((job) => (
+           <div key={job._id} className="bg-white rounded shadow-lg p-6">
+             <h2 className="text-xl font-bold mb-2">{job.title}</h2>
+             <p className="text-gray-600 mb-2">{job.deadline}</p>
+             <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
+             <p className="text-gray-700 mb-2">{job.shortDescription}</p>
+             <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
+               Bid Now
+             </button></Link>
+           </div>
+         ))}
+     </div>
+   </TabPanel>
+
+   <TabPanel>
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+       {jobs
+         .filter((job) => job.category === "graphics design")
+         .map((job) => (
+           <div key={job._id} className="bg-white rounded shadow-lg p-6">
+             <h2 className="text-xl font-bold mb-2">{job.title}</h2>
+             <p className="text-gray-600 mb-2">{job.deadline}</p>
+             <p className="text-gray-700 mb-2">{`Price Range: $${job.minPrice} - $${job.maxPrice}`}</p>
+             <p className="text-gray-700 mb-2">{job.shortDescription}</p>
+             <Link to={`/jobDetails/${job._id}`}><button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">
+               Bid Now
+             </button></Link>
+           </div>
+         ))}
+     </div>
+   </TabPanel>
+ </Tabs>
+</div>
+      }
+       
     </div>
     );
 };
--- src/Pages/SharedPages/Footer.jsx
+++ src/Pages/SharedPages/Footer.jsx
@@ -1,45 +1,45 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-10">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-                <div className="flex items-center mb-4 md:mb-0">
-                    <img src="https://i.ibb.co/V24V2tB/314948-removebg-preview.png" alt="Logo" className="w-16 h-16 mr-2" />
-                    <h1 className="text-xl font-bold">JobHunt</h1>
-                </div>
-                <div className="flex flex-wrap space-x-8 md:space-x-12">
-                    <a href="#contact" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
-                        Contact Us
-                    </a>
-                    <a href="#about" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
-                        About Us
-                    </a>
-                    <a href="#services" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
-                        Services
-                    </a>
-                    <a href="#why-choose-us" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
-                        Why Choose Us
-                    </a>
-                </div>
-                <div className="flex items-center space-x-4">
-                    <a href="mailto:info@jobhunt.com" className="text-gray-300 hover:text-white">
-                        info@jobhunt.com
-                    </a>
+        <footer className="bg-gray-800 text-white py-8 md:py-10">
+        <div className="container mx-auto flex flex-col md:flex-row justify-center md:justify-between items-center">
+            <div className="flex items-center mb-4 md:mb-0">
+                <img src="https://i.ibb.co/V24V2tB/314948-removebg-preview.png" alt="Logo" className="w-12 h-12 md:w-16 md:h-16 mr-2" />
+                <h1 className="text-xl font-bold">JobHunt</h1>
+            </div>
+            <div className="flex flex-wrap space-x-4 md:space-x-12 mb-4 md:mb-0">
+                <a href="#contact" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+                    Contact Us
+                </a>
+                <a href="#about" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+                    About Us
+                </a>
+                <a href="#services" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+                    Services
+                </a>
+                <a href="#why-choose-us" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+                    Why Choose Us
+                </a>
+            </div>
+            <div className="flex items-center space-x-4">
+                <a href="mailto:info@jobhunt.com" className="text-gray-300 hover:text-white">
+                    info@jobhunt.com
+                </a>
                 <div className="flex space-x-4">
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/1200px-2023_Facebook_icon.svg.png"
                         alt="Facebook Icon"
-                        className="w-6 h-6 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
+                        className="w-6 h-6 md:w-8 md:h-8 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
                     />
                     <img
                         src="https://cdn-icons-png.flaticon.com/512/3670/3670151.png"
                         alt="Twitter Icon"
-                        className="w-6 h-6 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
+                        className="w-6 h-6 md:w-8 md:h-8 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
                     />
                     <img
                         src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png"
                         alt="LinkedIn Icon"
-                        className="w-6 h-6 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
+                        className="w-6 h-6 md:w-8 md:h-8 cursor-pointer text-gray-300 hover:text-white transition duration-300 ease-in-out"
                     />
                 </div>
             </div>
